b45c540c3db9176ef14d761e88aaa819d1baaa2e
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2026-02-27 22:12:45 -0800

    feat: setup react frontend with tailwindcss, react-icons package

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+export default function App() {
+
+    return (
+        <main className="flex flex-col justify-center items-center py-2">
+            <h1 className="text-white text-5xl">Irvine Hacks 2026</h1>
+
+        </main>
+    )
+}
